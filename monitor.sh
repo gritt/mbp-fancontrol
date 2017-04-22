be9d65d6ac01b5cc57bfb57fc0eeddaec857734d
Gilvan <gilvanritter@gmail.com>
2017-04-22 17:10:03 -0300

    calculates new RPM based on temperature

--- monitor.sh
+++ monitor.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-INI_TEMP=62
-MAX_TEMP=82
+INI_TEMP=50
+MAX_TEMP=80
 
 MIN_RPM=2100
 MAX_RPM=5000
 SAV_RPM=5880
 
 
-# get current temperature
+# read current temperature
 cd /sys/devices/platform/applesmc.768
 CUR_TEMP="$( sensors | grep TPCD | grep -o '[0-9]*' | head -n1 )"
-echo "${CUR_TEMP}"
+
+
+echo "Current temperature ${CUR_TEMP} deegres"
 
 
 CUR_RPM=$MIN_RPM
-#CUR_TEMP=67
 
 
-if [ $CUR_TEMP -gt $INI_TEMP ]
-
+if [ $CUR_TEMP -gt $INI_TEMP ] 
 then
-    echo "Oi"
 
-    TMP="$(expr $CUR_TEMP * $MAX_RPM)"    
+    echo "Temperature is above recommended"
+
+    TMP=$((CUR_TEMP * MAX_RPM))
     
-    CUR_RPM="$( expr $TMP / $MAX_RPM )"  
-  
-    echo "${CUR_RPM}"
+    CUR_RPM=$((TMP / MAX_TEMP))
 fi
-echo "Tchau"
+
+echo "Adjusting fan speed to ${CUR_RPM} RPMs"
 
 
 # todo
